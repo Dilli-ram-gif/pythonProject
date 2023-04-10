5c291810ff4b97bfc17ef060a310eb8094f35cd2
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2023-04-10 09:12:05 +0545

    New project started

---
+++ game.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color('dark magenta')
+
+
+timmy.forward(100)
+timmy.pendown
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
